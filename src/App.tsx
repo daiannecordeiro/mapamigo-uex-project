9375f9e41066609c6aea1a1e10bf2d7570de2216
Daianne Cordeiro <daianne.nc@gmail.com>
2025-05-29 15:10:11 -0300

    feat: initialize project configuration

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import '@/config/styles/global.css';
+import mapamigoLogo from './assets/mapamigo-logo.svg'
+
+function App() {
+  return (
+    <>
+          <img src={mapamigoLogo} className="logo" alt="logo" />  
+          <h1>Seu gerenciador de endereços</h1>
+    </>
+  )
+}
+
+export default App
