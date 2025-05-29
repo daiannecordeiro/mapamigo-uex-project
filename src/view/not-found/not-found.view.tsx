835494c67b266721f38b713a2366dfa6a50ab868
Daianne Cordeiro <daianne.nc@gmail.com>
2025-05-29 17:32:48 -0300

    feat: add NotFound view

--- src/view/not-found/not-found.view.tsx
+++ src/view/not-found/not-found.view.tsx
@@ -1,12 +1,27 @@
 import { FC } from 'react'
+import styles from './not-found.module.css'
+import { NotFoundImage } from '@/assets'
+import { useNavigate } from '@/hooks'
+import { Button } from '@/components'
 
 const NotFound: FC = () => {
-    return (
-        <div className="not-found">
-        <h1>404 - Page Not Found</h1>
-        <p>The page you are looking for does not exist.</p>
-        </div>
-    )
+  const { navigate } = useNavigate()
+  const handleReturn = () => {
+    navigate('/login')
+  }
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>404 - Página não encontrada</h1>
+      <img className={styles.image} src={NotFoundImage} alt='Página não encontrada' />
+      <p className={styles.message}>A página que você está procurando não existe.</p>
+      <div className={styles.buttonContainer}>
+        <Button variant='filled' onClick={handleReturn} type='button'>
+          Ir para Login
+        </Button>
+      </div>
+    </div>
+  )
 }
 
 export default NotFound
