11176889dab08e44d22ff7986d64984fa49536e9
Daianne Cordeiro <daianne.nc@gmail.com>
2025-05-29 23:18:13 -0300

    feat: add ContactList component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,14 +1,19 @@
 // Common components
 export { default as PrivateRoute } from './common/private-route/private-route'
-export { default as LoginForm } from './common/forms/login-form/login-form'
-export { default as RegisterForm } from './common/forms/register-form/register-form'
 export { default as SnackBar } from './common/snackbar/snackbar'
 export { default as Header } from './common/header/header'
 export { default as Sidebar } from './common/sidebar/sidebar'
 export { default as MainPanel } from './common/main-panel/main-panel'
+
+// Common components - forms
+export { default as LoginForm } from './common/forms/login-form/login-form'
+export { default as RegisterForm } from './common/forms/register-form/register-form'
+export { default as AccountForm } from './common/forms/account-form/account-form'
 
 // MWC components
 export { default as Button } from './mwc/button/button'
 export { default as Input } from './mwc/input/input'
 export { default as Dialog } from './mwc/dialog/dialog'
 export { default as IconButton } from './mwc/icon-button/icon-button'
+export { default as ContactList } from './mwc/contact-list/contact-list'
+export { default as Switch } from './mwc/switch/switch'
